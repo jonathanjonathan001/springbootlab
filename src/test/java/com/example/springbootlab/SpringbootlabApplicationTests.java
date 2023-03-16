737985fc07bec1c1a496372564214072d2be9477
jonathanjonathan001 <60898344+jonathanjonathan001@users.noreply.github.com>
2023-03-16 10:48:28 +0100

    Comment out springbootlabapplicationtests

--- src/test/java/com/example/springbootlab/SpringbootlabApplicationTests.java
+++ src/test/java/com/example/springbootlab/SpringbootlabApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.springbootlab;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/*
 @SpringBootTest
 class SpringbootlabApplicationTests {
 
@@ -11,3 +12,4 @@ class SpringbootlabApplicationTests {
     }
 
 }
+*/
