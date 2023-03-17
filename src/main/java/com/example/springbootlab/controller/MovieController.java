3f090f0003951079c0f7a07a0154d506e5e8fd72
Ye4y <Arman.Alemirad@gmail.com>
2023-03-17 10:01:45 +0100

    Add post and get mapping

--- src/main/java/com/example/springbootlab/controller/MovieController.java
+++ src/main/java/com/example/springbootlab/controller/MovieController.java
@@ -1,5 +1,29 @@
 package com.example.springbootlab.controller;
 
+
+import com.example.springbootlab.entity.Movie;
+import com.example.springbootlab.repository.MovieRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/movies")
 public class MovieController {
+
+MovieRepository repo;
+
+public MovieController(MovieRepository movieRepository ){
+ repo = movieRepository;
 }
 
+@PostMapping
+    void addMovie (@RequestBody Movie movie){
+    repo.save(movie);
+}
+
+    @GetMapping
+    List<Movie> getAllMovies(){
+        return repo.findAll();
+    }
+}
