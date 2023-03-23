a99403dbe9cc08cd9057760900c99b4101a075f0
Anton20HD <116887276+Anton20HD@users.noreply.github.com>
2023-03-23 19:11:53 +0100

    Update Category.java

--- src/main/java/com/example/springbootlab/entity/Category.java
+++ src/main/java/com/example/springbootlab/entity/Category.java
@@ -3,6 +3,7 @@ package com.example.springbootlab.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import java.util.List;
 
