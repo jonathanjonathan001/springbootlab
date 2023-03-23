5878ff3144355cf1af766d3a58bcb2a512dfbd16
Ye4y <Arman.Alemirad@gmail.com>
2023-03-23 11:55:31 +0100

    Add tests post test

--- src/test/java/com/example/springbootlab/controller/MovieControllerTest.java
+++ src/test/java/com/example/springbootlab/controller/MovieControllerTest.java
@@ -3,13 +3,22 @@ package com.example.springbootlab.controller;
 import com.example.springbootlab.entity.Movie;
 import com.example.springbootlab.repository.MovieRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static net.bytebuddy.matcher.ElementMatchers.is;
+import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 import java.util.List;
@@ -26,12 +35,30 @@ class MovieControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-    @Autowired
-    private ObjectMapper objectMapper;
+   ObjectMapper objectMapper = new ObjectMapper();
+   ObjectWriter objectWriter = objectMapper.writer();
 
     @MockBean
     private MovieRepository repo;
 
+
+   @Test
+    void shouldCreateNewMovie() throws Exception {
+
+        Movie movie1 = new Movie();
+        movie1.setId(1L);
+        movie1.setName("The Lord of the Rings");
+        movie1.setYear(2001);
+
+        when(repo.save(any(Movie.class))).thenReturn(movie1);
+
+        mockMvc.perform(post("/movies")
+                .contentType(String.valueOf(MediaType.APPLICATION_JSON))
+                .content(objectMapper.writeValueAsString(movie1)))
+                .andExpect(status().isOk());
+
+
+    }
 
     @Test
     void getAllMoviesShouldReturnAllMoviesAnd200Ok() throws Exception {
